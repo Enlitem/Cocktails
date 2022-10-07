6e61a8a8b6e583562055c396daae259ef71d6691
Enlitem <eprimazov@yandex.ru>
2022-10-07 17:00:14 +0300

    Добавлен компонент кнопка

---
+++ src/components/MainButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="btn">
+    <button>{{ text }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainButton',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  @apply bg-pink py-[10px] flex justify-center;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 130%;
+  border-radius: 0 7px 7px 0;
+  color: #ffffff;
+}
+</style>
