41f4f11192332853d21759c5b295b7f2b38df76f
Enlitem <eprimazov@yandex.ru>
2022-10-07 16:59:26 +0300

    Настройка конфига tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '360px',
+      md: '768px',
+      lg: '1000px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Montserrat'],
+    },
+    colors: {
+      pink: '#EB2D47',
+      grey: '#443E3E',
+      'light-grey': '#999999',
+    },
   },
   plugins: [],
-}
+};
