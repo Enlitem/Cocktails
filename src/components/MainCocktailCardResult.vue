44756ea1a735e1b04d47830931ee9a2c229ae0dc
Enlitem <eprimazov@yandex.ru>
2022-10-29 21:03:10 +0300

    Результаты поиска коктейлей

---
+++ src/components/MainCocktailCardResult.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-wrap gap-y-[20px] gap-x-[20px] justify-center md:justify-between md:gap-x-[25px]">
+    <main-cocktail-card
+      class="last:mr-auto"
+      v-for="drink in drinks"
+      :name="drink.name"
+      :second-name="drink.secondName"
+      :ingredients="drink.ingredients"
+      :key="drink.name"
+    />
+  </div>
+</template>
+
+<script>
+import MainCocktailCard from '@/components/MainCocktailCard';
+export default {
+  name: 'MainCocktailCardResult',
+  components: { MainCocktailCard },
+  data() {
+    return {
+      drinks: [
+        {
+          name: 'Лонг Айленд Айс Ти',
+          secondName: 'Long Island Iced Tea',
+          ingredients: ['example', 'example', 'example', 'example', 'example'],
+        },
+        { name: 'Бостонский чай', secondName: 'Boston Tea', ingredients: ['example', 'example', 'example', 'example'] },
+        { name: 'Голубая лагуна', secondName: 'Blue Water', ingredients: ['example', 'example', 'example'] },
+        {
+          name: 'Бостонский чай',
+          secondName: 'Boston Tea',
+          ingredients: ['example', 'example', 'example', 'example', 'example'],
+        },
+        { name: 'Голубая лагуна', secondName: 'Blue Water', ingredients: ['example'] },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
