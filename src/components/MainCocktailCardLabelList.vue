ba73abe04baa36e096dd9442a5f53cbbf3fa4ae1
Enlitem <eprimazov@yandex.ru>
2022-10-29 21:02:19 +0300

    Теги коктейля

---
+++ src/components/MainCocktailCardLabelList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex gap-x-[6px] gap-y-[5px] flex-wrap">
+    <div
+      v-for="label in labels"
+      :key="label"
+      class="text-[10px] leading-[130%] font-normal text-light-grey py-0.5 px-2 rounded-[4px] border-[1px] border-light-grey"
+    >
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainCocktailCardLabelList',
+  props: {
+    labels: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
