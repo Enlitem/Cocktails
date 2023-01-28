dfc01443f9bb90887462f784617326d21cae9f4f
Enlitem <eprimazov@yandex.ru>
2023-01-28 15:19:16 +0300

    Появление навбара при скролле

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,37 +1,42 @@
 <template>
-  <nav class="flex items-center justify-between pt-[11px] pb-[10px] mb-[60px] lg:pt-[19px] lg:pb-[16px]">
-    <router-link to="/" exact>
-      <div class="logo">LOGO</div>
-    </router-link>
-    <div class="flex justify-between items-center">
-      <div class="hidden lg:block lg:mr-[144px]">
-        <ul class="flex">
-          <li class="nav-item">
-            <router-link to="/" exact>Главная</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/cocktails">Коктейли</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/party">Калькулятор вечеринок</router-link>
-          </li>
-        </ul>
-      </div>
-      <img class="mr-[3px]" src="@/assets/favourites.svg" alt="Favourites" />
-      <div class="lg:hidden">
-        <main-burger-menu>
-          <ul>
-            <li class="nav-item burger-item">
+  <nav
+    class="fixed z-50 left-0 right-0 px-5 w-[100vw] max-w-[1216px] m-auto pt-[11px] lg:pt-[19px]"
+    :class="{ 'navbar-hidden': !showNavbar }"
+  >
+    <div class="bordered flex items-center justify-between pb-[10px] lg:pb-[16px]">
+      <router-link to="/" exact>
+        <div class="logo">LOGO</div>
+      </router-link>
+      <div class="flex justify-between items-center">
+        <div class="hidden lg:block lg:mr-[144px]">
+          <ul class="flex">
+            <li class="nav-item">
               <router-link to="/" exact>Главная</router-link>
             </li>
-            <li class="nav-item burger-item">
+            <li class="nav-item">
               <router-link to="/cocktails">Коктейли</router-link>
             </li>
-            <li class="nav-item burger-item">
+            <li class="nav-item">
               <router-link to="/party">Калькулятор вечеринок</router-link>
             </li>
           </ul>
-        </main-burger-menu>
+        </div>
+        <img class="mr-[3px]" src="@/assets/favourites.svg" alt="Favourites" />
+        <div class="lg:hidden">
+          <main-burger-menu>
+            <ul>
+              <li class="nav-item burger-item">
+                <router-link to="/" exact>Главная</router-link>
+              </li>
+              <li class="nav-item burger-item">
+                <router-link to="/cocktails">Коктейли</router-link>
+              </li>
+              <li class="nav-item burger-item">
+                <router-link to="/party">Калькулятор вечеринок</router-link>
+              </li>
+            </ul>
+          </main-burger-menu>
+        </div>
       </div>
     </div>
   </nav>
@@ -42,13 +47,49 @@ import MainBurgerMenu from '@/components/MainBurgerMenu';
 export default {
   name: 'AppNavbar',
   components: { MainBurgerMenu },
+  data() {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0,
+    };
+  },
+  methods: {
+    onScroll() {
+      const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      if (currentScrollPosition < 0) {
+        return;
+      }
+      if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 50) {
+        return;
+      }
+      this.showNavbar = currentScrollPosition < this.lastScrollPosition;
+      this.lastScrollPosition = currentScrollPosition;
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  },
 };
 </script>
 
 <style scoped>
 nav {
+  background-color: #ffffff;
+  transform: translate3d(0, 0, 0);
+  transition: 0.1s all ease-out;
+}
+
+.navbar-hidden {
+  transform: translate3d(0, -100%, 0);
+}
+
+.bordered {
   border-bottom: 1px solid #cccccc;
 }
+
 .logo {
   font-style: normal;
   font-weight: 500;
